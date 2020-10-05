04df002f6d5560bd42323b79e17df5653846e86c
robertocuyar <roberto.cuyar44@gmail.com>
2020-10-05 13:51:07 -0500

    Last update to OOP exercise Part 2

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,9 +2,13 @@ package shapes;
 
 public class Circle {
     private double radius;
+    private static int counter = 0;
 
     public Circle (double radius){
         this.radius = radius;
+    }
+    public static int updateCount(){
+        return counter+= 1;
     }
     public double getArea(){
         return this.radius * this.radius * Math.PI;
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,18 +2,17 @@ package shapes;
 import util.Input;
 
 public class CircleApp {
-    private static int counter = 0;
     public static void CircleDisplay(){
         while (true) {
-           counter = counter++;
             Input radius = new Input();
             Circle counter = new Circle(radius.getDouble("Enter a radius: "));
+            int count = Circle.updateCount();
             System.out.println(counter.getArea());
             System.out.println(counter.getCircumference());
             Input response = new Input();
             boolean userInput = response.yesNo("Would you like to continue? y/N");
             if(!userInput) {
-                System.out.println("You have made " + counter + " circle(s).");
+                System.out.println(count);
                break;
                }
         }
