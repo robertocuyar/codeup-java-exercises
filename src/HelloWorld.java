5841681736b78654b5ca994006c48e34795761c4
robertocuyar <roberto.cuyar44@gmail.com>
2020-10-05 09:15:06 -0500

    small applications for references

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
+
 public class HelloWorld {
-    public static void main (String[] args){
-        int myFavoriteNumber = 4;
+    public static void main(String[] args) {
         String myString = "What?";
         float myNumber = 3.14F;
         System.out.println("Hello, World!");
-        System.out.println(myFavoriteNumber);
+        //System.out.println(myFavoriteNumber);
         System.out.println(myString);
         System.out.println(myNumber);
         /*int x = 5;
@@ -19,15 +19,15 @@ public class HelloWorld {
         //int three = (int) o;
         //int three = (int) "three";
         System.out.println(o);
-        /*int x = 4;
+        int x = 4;
         x += 5;
         System.out.println(x);
-        int x = 3;
+        //int x = 3;
         int y = 4;
         y *= x;
-        System.out.println(y);*/
-        int x = 10;
-        int y = 2;
+        System.out.println(y);
+        //int x = 10;
+        //int y = 2;
         x /= y;
         y -= x;
         System.out.println(y);
