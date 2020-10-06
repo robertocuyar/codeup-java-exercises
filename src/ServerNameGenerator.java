4aa8cb372c8c7ac54def07e37ca29af8111c738a
robertocuyar <roberto.cuyar44@gmail.com>
2020-10-06 12:50:52 -0500

    Array exercise part 2

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,15 @@
+import java.lang.Math;
+
 public class ServerNameGenerator {
+
+
+    public static void main(String[] args) {
+        String[] adjectives = {"uncovered", "roasted", "lucky", "rare", "shivering", "faulty", "soggy", "dangerous", "waggish", "silly"};
+        String[] nouns = {"breath", "dime", "bed", "anger", "ants", "plot", "step", "rainstorm", "eyes", "basket"};
+        int randomIndex = (int) (Math.random() * (10));
+        int randomIndex2 = (int) (Math.random() * (10));
+        System.out.printf("Here is your server name:\n %s-%s", adjectives[randomIndex], nouns[randomIndex2]);
+    }
+
 }
+
