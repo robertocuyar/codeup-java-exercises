778ea3a1f25fe37dc7c05311b505a2381cf1ac20
robertocuyar <roberto.cuyar44@gmail.com>
2020-10-06 15:06:35 -0500

    Array exercise part 3 bonus

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,13 +1,15 @@
 package movies;
-import java.util.Scanner;
 import util.Input;
+
+
+import java.util.Arrays;
 
 public class MoviesApplication {
     public static void main(String[] args) {
-runApp();
+runApp(MoviesArray.findAll());
 
     }
-    public static void runApp (){
+    public static void runApp (Movie[] array){
         System.out.println("What would you like to do?");
         System.out.printf("%n");
         System.out.println("0 - exit");
@@ -16,6 +18,7 @@ runApp();
         System.out.println("3 - view movies in the drama category");
         System.out.println("4 - view movies in the horror category");
         System.out.println("5 - view movies in the scifi category");
+        System.out.println("6 - add a movie to the list");
         System.out.printf("%n");
         System.out.print("Enter your choice: ");
         Input response = new Input();
@@ -26,62 +29,72 @@ runApp();
                 break;
             case "1":
                 System.out.printf("%n");
-                for (Movie movie : MoviesArray.findAll()){
+                for (Movie movie : array){
                     System.out.printf("%s -- %s\n", movie.getName(),movie.getCategory());
                 }
                 System.out.printf("%n");
-                runApp();
+                runApp(array);
                 break;
             case "2":
                 System.out.printf("%n");
-                for (Movie movie : MoviesArray.findAll()){
+                for (Movie movie : array){
                     if(movie.getCategory().equals("animated")) {
                         System.out.printf("%s -- %s\n", movie.getName(), movie.getCategory());
                     }
                 }
                 System.out.printf("%n");
-                runApp();
+                runApp(array);
                 break;
             case "3":
                 System.out.printf("%n");
-                for (Movie movie : MoviesArray.findAll()){
+                for (Movie movie : array){
                     if(movie.getCategory().equals("drama")) {
                         System.out.printf("%s -- %s\n", movie.getName(), movie.getCategory());
                     }
                 }
                 System.out.printf("%n");
-                runApp();
+                runApp(array);
                 break;
             case "4":
                 System.out.printf("%n");
-                for (Movie movie : MoviesArray.findAll()){
+                for (Movie movie : array){
                     if(movie.getCategory().equals("horror")) {
                         System.out.printf("%s -- %s\n", movie.getName(), movie.getCategory());
                     }
                 }
                 System.out.printf("%n");
-                runApp();
+                runApp(array);
                 break;
             case "5":
                 System.out.printf("%n");
-                for (Movie movie : MoviesArray.findAll()){
+                for (Movie movie : array){
                     if(movie.getCategory().equals("scifi")) {
                         System.out.printf("%s -- %s\n", movie.getName(), movie.getCategory());
                     }
                 }
                 System.out.printf("%n");
-                runApp();
+                runApp(array);
+                break;
+            case "6":
+                System.out.printf("%n");
+                String userMovie = response.getString("Enter a movie name: ");
+                String userCat = response.getString("Enter a category for the movie: ").toLowerCase();
+               Movie add  = new Movie (userMovie, userCat);
+                System.out.printf("%n");
+                runApp(addInfo(array, add));
                 break;
             default:
                 System.out.printf("%n");
                 System.out.println("Please enter a valid number.");
                 System.out.printf("%n");
-                runApp();
+                runApp(array);
                 break;
         }
-
-
-
-
+    }
+    public static Movie[] addInfo(Movie[] array, Movie plus) {
+        int changeLength = array.length + 1;
+        Movie[] outArray = Arrays.copyOf(array, changeLength);
+        outArray[changeLength - 1] = plus;
+        return outArray;
     }
 }
